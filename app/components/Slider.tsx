4f7e67d5e3fb00f6771da54f944dd3f314a3ef89
Murad <murad@MacBook-Pro-Murad.local>
2026-02-22 16:50:07 +0400

    a

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -12,7 +12,7 @@ export default function Slider({ href }: Props) {
     <div className="w-full">
       <Link href={href}>
         <Image
-          src={"/ml.png"}
+          src={"/pb.png"}
           alt={`poster`}
           width={2000}
           height={2000}
