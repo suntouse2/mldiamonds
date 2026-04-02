e4a0af5f1672995c0b497ea6c4ab34de3ebad474
Murad <murad@MacBook-Pro-Murad.local>
2026-04-02 22:19:47 +0400

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata, Viewport } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import ToasterWrapper from "./ui/ToasterWrapper";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ["latin", "cyrillic"],
@@ -30,6 +32,35 @@ export default function RootLayout({
       <body className="antialiased">
         <ToasterWrapper />
         {children}
+        <Script id="yandex-metrika-108368926" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108368926','ym');
+
+            ym(108368926, 'init', {
+              webvisor:true,
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/108368926"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
         <div id="portal-root"></div>
       </body>
     </html>
